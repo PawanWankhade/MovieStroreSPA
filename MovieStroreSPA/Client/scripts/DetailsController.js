138da94f632c730ddb1d5f67a2869af8523f4d1a
Pawan Wankhade <pawanwankhade@hotmail.com>
2016-09-23 12:26:22 +0530

    Correcting the EditController changes
    Updated code from verified code module.

--- MovieStroreSPA/Client/scripts/DetailsController.js
+++ MovieStroreSPA/Client/scripts/DetailsController.js
@@ -5,4 +5,17 @@
     $http.get("/api/movie/" + id).success(function (data) {
         $scope.movie = data;
     });
+    
+     $scope.Edit = function (m) {
+
+        $scope.edit = {
+            movie:
+        {
+            Title: "",
+            RunTime: 0,
+            ReleaseYear: new Date().getFullYear()
+        }
+        };
+        $scope.edit.movie = angular.copy(m);
+    };
 });
